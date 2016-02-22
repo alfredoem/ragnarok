f33acae1cf403d447803857ed5bec528f5a28452
aespiritu <espiritumalfredo@gmail.com>
2016-02-22 10:00:59 -0500

    Security Migration - secMenus - secRoles - secRoleMenus - secRoleUsers - secActions - secActionsLog

--- resources/stubs/database/migrations/2016_01_01_000000_create_security_tables.php
+++ resources/stubs/database/migrations/2016_01_01_000000_create_security_tables.php
@@ -18,6 +18,12 @@ class CreateSecurityTables extends Migration
         $this->secParameters();
         $this->secApps();
         $this->secUserSessions();
+        $this->secMenus();
+        $this->secRoles();
+        $this->secRoleMenus();
+        $this->secRoleUsers();
+        $this->secActions();
+        $this->secActionsLog();
     }
 
     public function secParameters()
@@ -27,10 +33,10 @@ class CreateSecurityTables extends Migration
             $table->string('name', 50);
             $table->string('description');
             $table->string('value');
-            $table->integer('userIns');
+            $table->unsignedInteger('userIns');
             $table->date('dateIns')->default('0000-00-00');
             $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
-            $table->integer('userUpd');
+            $table->unsignedInteger('userUpd');
             $table->date('dateUpd')->default('0000-00-00');
             $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
         });
@@ -38,13 +44,18 @@ class CreateSecurityTables extends Migration
         DB::table('SecParameters')->insert([
             [
                 'name' => 'API_SECURITY_URL',
-                'description' => 'URL del API de seguridad',
+                'description' => 'Security API url',
                 'value'       => 'http://172.16.11.237/crona/public/api/v1'
             ],
             [
                 'name' => 'SERVER_SECURITY_URL',
-                'description' => 'URL del servidor de seguridad',
+                'description' => 'Security SERVER url',
                 'value'       => 'http://172.16.11.237/crona/public'
+            ],
+            [
+                'name' => 'MAX_LOGIN_ATTEMPTS',
+                'description' => 'The maximum number of login attempts for delaying further attempts',
+                'value'       => '5'
             ]
         ]);
     }
@@ -59,13 +70,13 @@ class CreateSecurityTables extends Migration
             $table->string('firstName', 50);
             $table->char('changePassword', 1);
             $table->dateTime('lastPasswordChange')->default('0000-00-00 00:00:00');
-            $table->tinyInteger('invalidAttempts', false);
-            $table->tinyInteger('status', false);
+            $table->unsignedTinyInteger('invalidAttempts', false);
+            $table->unsignedTinyInteger('status', false);
             $table->rememberToken();
-            $table->integer('userIns');
+            $table->unsignedInteger('userIns');
             $table->date('dateIns')->default('0000-00-00');
             $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
-            $table->integer('userUpd');
+            $table->unsignedInteger('userUpd');
             $table->date('dateUpd')->default('0000-00-00');
             $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
         });
@@ -84,14 +95,14 @@ class CreateSecurityTables extends Migration
             $table->increments('appId');
             $table->string('name', 100);
             $table->text('description');
-            $table->tinyInteger('type');
+            $table->unsignedTinyInteger('type');
             $table->string('logo', '250');
             $table->string('url', '250');
-            $table->tinyInteger('status')->unsigned();
-            $table->integer('userIns');
+            $table->unsignedTinyInteger('status');
+            $table->unsignedInteger('userIns');
             $table->date('dateIns')->default('0000-00-00');
             $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
-            $table->integer('userUpd');
+            $table->unsignedInteger('userUpd');
             $table->date('dateUpd')->default('0000-00-00');
             $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
         });
@@ -120,13 +131,112 @@ class CreateSecurityTables extends Migration
     {
         Schema::create('SecUserSessions', function(Blueprint $table){
             $table->increments('userSessionId');
-            $table->integer('userId');
+            $table->unsignedInteger('userId');
             $table->char('sessionCode', 15);
             $table->string('ipAddress', 50);
             $table->char('status', 1)->default(1);
             $table->date('dateIns')->default('0000-00-00');
             $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
             $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    public function secMenus()
+    {
+        Schema::create('SecMenus', function(Blueprint $table){
+            $table->increments('menuId');
+            $table->unsignedInteger('menuParentId');
+            $table->unsignedInteger('appId');
+            $table->char('isChild', 1)->default(0);
+            $table->string('name', 50);
+            $table->text('description');
+            $table->unsignedTinyInteger('position');
+            $table->char('division', 1);
+            $table->string('icon', 100);
+            $table->string('route', 500);
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedInteger('userIns');
+            $table->date('dateIns')->default('0000-00-00');
+            $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
+            $table->unsignedInteger('userUpd');
+            $table->date('dateUpd')->default('0000-00-00');
+            $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    public function secRoles()
+    {
+        Schema::create('SecRoles', function(Blueprint $table){
+            $table->increments('roleId');
+            $table->unsignedInteger('appId');
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedTinyInteger('level');
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedInteger('userIns');
+            $table->date('dateIns')->default('0000-00-00');
+            $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
+            $table->unsignedInteger('userUpd');
+            $table->date('dateUpd')->default('0000-00-00');
+            $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    public function secRoleMenus()
+    {
+        Schema::create('SecRoleMenus', function(Blueprint $table){
+            $table->unsignedInteger('roleId');
+            $table->unsignedInteger('menuId');
+            $table->unsignedTinyInteger('status');
+            $table->unsignedInteger('userIns');
+            $table->date('dateIns')->default('0000-00-00');
+            $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
+            $table->unsignedInteger('userUpd');
+            $table->date('dateUpd')->default('0000-00-00');
+            $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    public function secRoleUsers()
+    {
+        Schema::create('SecRoleUsers', function(Blueprint $table) {
+            $table->unsignedInteger('userId');
+            $table->unsignedInteger('roleId');
+            $table->unsignedTinyInteger('status')->default(1);
+            $table->unsignedInteger('userIns');
+            $table->date('dateIns')->default('0000-00-00');
+            $table->dateTime('datetimeIns')->default('0000-00-00 00:00:00');
+            $table->unsignedInteger('userUpd');
+            $table->date('dateUpd')->default('0000-00-00');
+            $table->dateTime('datetimeUpd')->default('0000-00-00 00:00:00');
+        });
+    }
+
+    public function secActions()
+    {
+        Schema::create('SecActions', function(Blueprint $table){
+           $table->char('actionCode', 50);
+           $table->string('actionName', 100);
+        });
+    }
+
+    public function secActionsLog()
+    {
+        Schema::create('SecActionsLog', function(Blueprint $table){
+           $table->increments('actionLogId');
+           $table->unsignedInteger('appId')->default(0);
+           $table->unsignedInteger('userId')->default(0);
+           $table->unsignedInteger('menuId')->default(0);
+           $table->unsignedInteger('actionId')->default(0);
+           $table->text('beforeAction');
+           $table->text('afterAction');
+           $table->char('ipAddress', 50);
+           $table->tinyInteger('actionResult');
+           $table->date('logDate')->default('0000-00-00');
+           $table->unsignedSmallInteger('logDateYear')->default(0);
+           $table->unsignedSmallInteger('logDateMonth')->default(0);
+           $table->unsignedSmallInteger('logDateHour')->default(0);
+           $table->datetime('logDatetime')->default('0000-00-00 00:00:00');
         });
     }
 
@@ -141,5 +251,11 @@ class CreateSecurityTables extends Migration
         Schema::drop('SecParameters');
         Schema::drop('SecApps');
         Schema::drop('SecUserSessions');
+        Schema::drop('SecMenus');
+        Schema::drop('SecRoles');
+        Schema::drop('SecRoleMenus');
+        Schema::drop('SecRoleUsers');
+        Schema::drop('SecActions');
+        Schema::drop('SecActionsLog');
     }
 }
